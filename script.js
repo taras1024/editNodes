493ec8bdd02e1b7ed03a729cdf891d5db6a282a6
Holynskyi <mnetworks10@gmail.com>
2022-02-01 14:58:59 +0200

    change base URL to make universal version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const snippetMainContainer = document.createElement('div')
 const snippetNavContainer = document.createElement('div')
-const snippetUrl = document.createElement('input')
+const snippetUrlInput = document.createElement('input')
 const snippetGoBtn = document.createElement('button')
 const snippetPageView = document.createElement('iframe')
 snippetPageView.id = "snippetIframe"
@@ -11,101 +11,122 @@ snippetMainContainer.style.cssText = `position: fixed; display:flex; align-items
 												 z-index: 999999; background-color: #121a23; `
 snippetNavContainer.style.cssText = `width: 100%;  padding: 20px 5px; background-color: #0f0f0f; display: flex; justify-content: center;`
 
-snippetUrl.style.cssText = `border-radius: 20px 0 0 20px; padding:  5px 20px; text-align: center; outline: none;
+snippetUrlInput.style.cssText = `border-radius: 20px 0 0 20px; padding:  5px 20px; text-align: center; outline: none;
 									 color: #fff; border: 1px solid #69aa00; background-color: #0f0f0f; width: 70%`
 
 
-snippetUrl.style.cssText = `border-radius: 20px 0 0 20px; padding:  5px 20px; text-align: center; outline: none;
+snippetUrlInput.style.cssText = `border-radius: 20px 0 0 20px; padding:  5px 20px; text-align: center; outline: none;
                                      color: #fff; border: 1px solid #69aa00; background-color: #0f0f0f; width: 70%`
- 
+
 snippetGoBtn.style.cssText = `border-radius: 0 20px 20px 0; padding:  5px 20px; text-align: center;
 									 color: #fff; border: 1px solid #69aa00; background-color: #69aa00; outline: none;`
 
 snippetPageView.style.cssText = `width:100%; height: calc(100% - 146px); `
 
 
-snippetUrl.value = 'URL'
+snippetUrlInput.value = 'URL'
 snippetGoBtn.innerHTML = 'RUN SCRIPT'
 
 
-snippetNavContainer.append(snippetUrl)
+snippetNavContainer.append(snippetUrlInput)
 snippetNavContainer.append(snippetGoBtn)
 snippetMainContainer.append(snippetNavContainer)
 snippetMainContainer.append(snippetPageView)
 
 document.querySelector('body').appendChild(snippetMainContainer)
 
+let baseURL = 'https://live-dig0030184-petcare-purinattt-spain.pantheonsite.io'
 
 
-snippetPageView.setAttribute('src', `https://live-dig0028577-petcare-purinattt-austria.pantheonsite.io/admin/content`)
+snippetPageView.setAttribute('src', `${baseURL}/admin/content`)
 // snippetPageView.setAttribute('src', `https://uat-74995-petcare-purinattt-unitedkingdom.pantheonsite.io/admin/content`)
 
 
 
 function changePage() {
-	const url = snippetPageView.contentWindow.location.href
-	snippetUrl.value = url
+    const url = snippetPageView.contentWindow.location.href
+    snippetUrlInput.value = url
 }
 
-snippetPageView.addEventListener('load', async () => {
-	changePage()
-})
-
 snippetGoBtn.addEventListener('click', () => {
-    startEditNodes()
+    // startEditNodes()
 })
 
-const nodeIdStr = `6001
-5996
-5296
-5291`
+count = 0
+
+snippetPageView.addEventListener('load', function () {
+    changePage()
+
+    if (nodeIdArr1[count]) {
+        if (snippetPageView.src.includes('edit')) {
+            editNode()
+            setTimeout(() => {
+                console.log(`${count} FROM EVENT TIMEOUT`, snippetPageView.src.slice(snippetPageView.src.indexOf('node')))
+                count++
+                if (nodeIdArr1[count]) {
+                    snippetPageView.src = `https://live-dig0028577-petcare-purinattt-austria.pantheonsite.io/en/node/${nodeIdArr1[count]}/edit`
+                }
+            }, 500)
+            console.log('FROM EVENT', snippetPageView.src.slice(snippetPageView.src.indexOf('node')))
+        } else {
+            count++
+            snippetPageView.src = `https://live-dig0028577-petcare-purinattt-austria.pantheonsite.io/en/node/${nodeIdArr1[count]}/edit`
+        }
+    }
+
+})
+
+// const nodeIdStr = `6001
+// 5996
+// 5296
+// 5291`
+
+// const nodeIdArr = nodeIdStr.split('\n')
 
 
-const nodeIdArr = nodeIdStr.split('\n')
+let nodeIdStr1
+
+// const nodeIdArr1 = nodeIdStr1.split('\n')
 
 
-// async function startEditNodes () {
-//     let nodeLink = ''
-//     for(let i = 0; i < 3; i++) {
-//         nodeLink = `https://uat-74995-petcare-purinattt-unitedkingdom.pantheonsite.io/node/${nodeIdArr[i]}/edit`
-//         // snippetPageView.onload = async () => {
-//         //     console.log (i, nodeIdArr[i])
-//         //     await editNode()
-//         // }   
-//         snippetPageView.contentWindow.location.href = nodeLink 
-//         console.log(snippetPageView.contentWindow.document.readyState)
-//         while(snippetPageView.contentWindow.document.readyState != 'complete') {
-//             console.log(snippetPageView.contentWindow.document.readyState)
-//         }
-//         // snippetPageView.src = nodeLink
-//     	// snippetUrl.value = nodeLink
-        
-//         // await editNode()
-        
-//     }
-// }
+// snippetPageView.src = `https://live-dig0028577-petcare-purinattt-austria.pantheonsite.io/en/node/${nodeIdArr1[count]}/edit`
 
-// async function editNode () {
-//     // console.log(`nodeLink: ${link}`)
+function editNode() {
+    const buyNowFusepump = snippetPageView.contentWindow.document.getElementById('edit-field-product-fusepump-0-value')
+    const buyNowLink = snippetPageView.contentWindow.document.getElementById('edit-field-product-buy-link-0-uri')
+    const bvID = snippetPageView.contentWindow.document.getElementById('edit-field-product-bv-id-0-value')
 
-//     const buyNowFusepump = snippetPageView.contentWindow.document.getElementById('edit-field-product-fusepump-wrapper')
-//     const buyNowLink = snippetPageView.contentWindow.document.getElementById('edit-field-product-buy-link-wrapper')
-//     const firstTab = snippetPageView.contentWindow.document.querySelector('.horizontal-tab-button-0 a')
-//     const saveBtn = snippetPageView.contentWindow.document.getElementById('edit-submit')
+    const generalTab = snippetPageView.contentWindow.document.querySelector('.horizontal-tab-button-0 a')
+    const relatedProdTab = snippetPageView.contentWindow.document.querySelector('.horizontal-tab-button-4 a')
 
-//     if(firstTab) {
-//         firstTab.click()
-//     }
+    const relatedProd1 = snippetPageView.contentWindow.document.getElementById('edit-field-product-related-products-0-target-id')
+    const relatedProd2 = snippetPageView.contentWindow.document.getElementById('edit-field-product-related-products-1-target-id')
+    const relatedProd3 = snippetPageView.contentWindow.document.getElementById('edit-field-product-related-products-2-target-id')
+    const relatedProd4 = snippetPageView.contentWindow.document.getElementById('edit-field-product-related-products-3-target-id')
 
-//     snippetPageView.contentWindow.scrollTo(0,600)
-    
-//     buyNowFusepump ? await editInput(buyNowFusepump) : console.log('Fusepump absent')
-//     buyNowLink ? await editInput(buyNowFusepump) : console.log('Buy now link absent')
-//     // saveBtn ? saveBtn.click() : console.log('saveBtn absent')
-// }
+    const saveBtn = snippetPageView.contentWindow.document.getElementById('edit-submit')
 
+    if (generalTab) {
+        generalTab.click()
+    }
 
-// async function editInput(node) {
-// 	const nodeInput = node.querySelector('input')
-// 	nodeInput.value = nodeInput.value + 'E'
-// }
+    snippetPageView.contentWindow.scrollTo(0, 500)
+
+    buyNowFusepump ? buyNowFusepump.value = '' : console.log('Fusepump absent')
+    buyNowLink ? buyNowLink.value = '' : console.log('Buy now link absent')
+    bvID ? bvID.value = '' : console.log('bvID absent')
+
+    //     if(relatedProdTab) {
+    //         relatedProdTab.click()
+    //     }
+
+    relatedProd1 ? relatedProd1.value = '' : console.log('relatedProd1 absent')
+    relatedProd2 ? relatedProd2.value = '' : console.log('relatedProd2 absent')
+    relatedProd3 ? relatedProd3.value = '' : console.log('relatedProd3 absent')
+    relatedProd4 ? relatedProd4.value = '' : console.log('relatedProd4 absent')
+
+    snippetPageView.contentWindow.confirm = function () { return true; }
+    window.confirm = function () { return true; }
+
+    saveBtn ? saveBtn.click() : console.log('saveBtn absent')
+}

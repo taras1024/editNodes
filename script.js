c18d7286721ec24a548c60401533a2592c07ef1e
Holynskyi <mnetworks10@gmail.com>
2022-02-01 16:14:17 +0200

    fix arr

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ snippetPageView.addEventListener('load', function () {
 
 let nodeIdStr1 = ``
 
-const nodeIdArr1 = nodeIdStr1.split('\n')
+let nodeIdArr1 = nodeIdStr1.split('\n')
 
 
 // snippetPageView.src = `https://live-dig0028577-petcare-purinattt-austria.pantheonsite.io/en/node/${nodeIdArr1[count]}/edit`

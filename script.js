7ffed29824474517292cbb8ec4c893cf87d77fa5
Holynskyi <mnetworks10@gmail.com>
2022-02-02 10:57:23 +0200

    adjusting script to different cases

--- script.js
+++ script.js
@@ -1,44 +1,96 @@
+const bodyNode = document.querySelector('body')
+
 const snippetMainContainer = document.createElement('div')
+const snippetControlContainer = document.createElement('div')
+const snippetControlLeftSide = document.createElement('div')
+const snippetControlRightSide = document.createElement('div')
+const snippetInput = document.createElement('TEXTAREA')
+const snippetBtnContainer = document.createElement('div')
+// const snippetAddCustomBtn = document.createElement('button')
+const snippetRunBtn = document.createElement('button')
+const snippetGoBtn = document.createElement('button')
+
+const snippetLoaderContainer = document.createElement('div')
+
+const snippetPageView = document.createElement('iframe')
 const snippetNavContainer = document.createElement('div')
 const snippetUrlInput = document.createElement('input')
-const snippetGoBtn = document.createElement('button')
-const snippetPageView = document.createElement('iframe')
-snippetPageView.id = "snippetIframe"
 
+
+
+const snippetBtnStyle =
+    'outline: none; border: 0; border-radius: 20px; padding: 10px 20px; cursor:pointer; color: white; margin: 0 5px; line-height: 10px; outline: none;'
+
+
+// snippetAddCustomBtn.id = 'snippetAddCustomBtn'
+// snippetAddCustomBtn.innerHTML = 'Add custom'
+// snippetAddCustomBtn.style.cssText = `background-color: #69aa00; ${snippetBtnStyle}`
+
+snippetRunBtn.id = 'snippetRunBtn'
+snippetRunBtn.innerHTML = 'RUN'
+snippetRunBtn.style.cssText = `background-color: red; ${snippetBtnStyle}`
+
+snippetInput.placeholder = 'Insert links here...'
+
+snippetBtnContainer.style.cssText = ` padding: 0 10px; display:flex;  justify-content:space-between`
 
 snippetMainContainer.style.cssText = `position: fixed; display:flex; align-items: center;
 												 left: 0; top: 0; width:100%; height: 100%; flex-direction: column;
 												 z-index: 999999; background-color: #121a23; `
+
+snippetControlContainer.style.cssText = `width:100%; height:220px; display:flex; padding: 0 5px`
+
+snippetControlLeftSide.style.cssText = `display: flex; justify-content: center; align-items: center; padding: 7px 0;`
+
+snippetPageView.style.cssText = `width:100%; height: calc(100% - 146px); `
+
 snippetNavContainer.style.cssText = `width: 100%;  padding: 20px 5px; background-color: #0f0f0f; display: flex; justify-content: center;`
 
-snippetUrlInput.style.cssText = `border-radius: 20px 0 0 20px; padding:  5px 20px; text-align: center; outline: none;
+snippetUrlInput.style.cssText = `border-radius: 20px 0 0 20px; padding:  10px 20px; text-align: center; outline: none;
 									 color: #fff; border: 1px solid #69aa00; background-color: #0f0f0f; width: 70%`
-
-
-snippetUrlInput.style.cssText = `border-radius: 20px 0 0 20px; padding:  5px 20px; text-align: center; outline: none;
-                                     color: #fff; border: 1px solid #69aa00; background-color: #0f0f0f; width: 70%`
 
 snippetGoBtn.style.cssText = `border-radius: 0 20px 20px 0; padding:  5px 20px; text-align: center;
 									 color: #fff; border: 1px solid #69aa00; background-color: #69aa00; outline: none;`
 
-snippetPageView.style.cssText = `width:100%; height: calc(100% - 146px); `
+snippetInput.style.cssText = `width: 400px; height: 200px; border-radius:  20px; resize: none; padding: 4px 10px; outline: none; background-color: #0f0f0f;
+										 border: 1px solid #00385a; color: #fff;`
 
+
+snippetPageView.id = "snippetIframe"
 
 snippetUrlInput.value = 'URL'
-snippetGoBtn.innerHTML = 'RUN SCRIPT'
+snippetGoBtn.innerHTML = 'GO'
 
+snippetControlContainer.append(snippetControlLeftSide)
+snippetControlContainer.append(snippetControlRightSide)
+
+snippetControlLeftSide.append(snippetInput, snippetBtnContainer)
+snippetBtnContainer.append(
+    // snippetAddCustomBtn,
+    snippetRunBtn
+)
 
 snippetNavContainer.append(snippetUrlInput)
 snippetNavContainer.append(snippetGoBtn)
+// snippetLoaderContainer.append(snippetLoader)
+// snippetLoaderContainer.append(snippetLoaderMessage)
+
+snippetMainContainer.append(snippetControlContainer)
 snippetMainContainer.append(snippetNavContainer)
+// snippetMainContainer.append(snippetLoaderContainer)
 snippetMainContainer.append(snippetPageView)
 
-document.querySelector('body').appendChild(snippetMainContainer)
+bodyNode.append(snippetMainContainer)
+
+
 
 let baseURL = 'https://live-dig0030184-petcare-purinattt-spain.pantheonsite.io'
+let nodeIdStr1 = ``
+let nodeIdArr1 = nodeIdStr1.split('\n')
+let count = 0
 
 
-snippetPageView.setAttribute('src', `${baseURL}/admin/content`)
+// snippetPageView.setAttribute('src', `${baseURL} /admin/content`)
 // snippetPageView.setAttribute('src', `https://uat-74995-petcare-purinattt-unitedkingdom.pantheonsite.io/admin/content`)
 
 
@@ -47,11 +99,20 @@ function changePage() {
     snippetUrlInput.value = url
 }
 
-snippetGoBtn.addEventListener('click', () => {
-    // startEditNodes()
+// snippetGoBtn.addEventListener('click', () => {
+//     startEditNodes()
+// })
+
+snippetRunBtn.addEventListener('click', () => {
+    count = 0
+    baseURL = snippetUrlInput.value
+
+    nodeIdStr1 = snippetInput.value
+    nodeIdArr1 = nodeIdStr1.split('\n')
+
+    snippetPageView.setAttribute('src', `${baseURL} /admin/content`)
 })
 
-count = 0
 
 snippetPageView.addEventListener('load', function () {
     changePage()
@@ -75,17 +136,8 @@ snippetPageView.addEventListener('load', function () {
 
 })
 
-// const nodeIdStr = `6001
-// 5996
-// 5296
-// 5291`
-
-// const nodeIdArr = nodeIdStr.split('\n')
 
 
-let nodeIdStr1 = ``
-
-let nodeIdArr1 = nodeIdStr1.split('\n')
 
 
 // snippetPageView.src = `https://live-dig0028577-petcare-purinattt-austria.pantheonsite.io/en/node/${nodeIdArr1[count]}/edit`
